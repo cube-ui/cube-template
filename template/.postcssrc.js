67f710f57023d08026c58c620a3b29018c5fc820
doly mood <dolymood@gmail.com>
2017-12-15 17:29:37 +0800

    support rem layout (#3)
    * support rem layout
    
    * style extract = false
    
    * support rem layout

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -4,6 +4,9 @@ module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
     "postcss-import": {},
-    "autoprefixer": {}
+    "autoprefixer": {}{{#rem}},
+    "postcss-px2rem": {
+      "remUnit": 37.5
+    }{{/rem}}
   }
 }
--- template/src/main.js
+++ template/src/main.js
@@ -1,3 +1,6 @@
+{{#amfeFlexible}}
+import 'amfe-flexible'
+{{/amfeFlexible}}
 {{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
