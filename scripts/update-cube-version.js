2f02cbe15bdc3b42706ea65882e11d0779adac94
AmyFoxFN <funan_amy@foxmail.com>
2018-06-17 21:37:48 +0800

    [workflow] get latest version

--- scripts/update-cube-version.js
+++ scripts/update-cube-version.js
@@ -1,10 +1,43 @@
-var fs = require('fs')
-var path = require('path')
-var version = process.argv[2]
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
 
-var packagePath = path.resolve(__dirname, '../template/package.json')
-var content = fs.readFileSync(packagePath).toString()
+const options = {
+  hostname: 'registry.npmjs.org',
+  port: 443,
+  path: '/cube-ui',
+  method: 'GET'
+}
 
-content = content.replace(/(?<="cube-ui": "~)\d+\.\d+\.\d+(?=")/, version)
+// Get latest version of cube-ui
+const req = https.request(options, (res) => {
+  if (res.statusCode !== 200) {
+    console.error(`Request Failed. Status Code: ${statusCode}`)
+    res.resume()
 
-fs.writeFileSync(packagePath, content)
+    return
+  }
+
+  let rawData = ''
+  res.on('data', (data) => {
+    rawData += data
+  })
+  res.on('end', () => {
+    const parsedData = JSON.parse(rawData)
+    const version = parsedData['dist-tags'].latest
+    replaceVersion(version)
+  })
+})
+
+req.on('error', (e) => {
+  console.error(e);
+})
+
+req.end()
+
+function replaceVersion(version) {
+  const packagePath = path.resolve(__dirname, '../template/package.json')
+  let content = fs.readFileSync(packagePath).toString()
+  content = content.replace(/(?<="cube-ui": "~)\d+\.\d+\.\d+(?=")/, version)
+  fs.writeFileSync(packagePath, content)
+}
